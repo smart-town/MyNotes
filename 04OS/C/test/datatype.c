4c076a630e2c5436d643dcd010108e7c49178998
smalltown <1213408659@qq.com>
2019-06-27 21:30:16 +0800

    c...

---
+++ 04OS/C/test/datatype.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+// #define SIZ(x) SIZEOF(x)
+int main(){
+    printf("sizof(int)%ld\n",sizeof(char));
+
+    char a = 127+1;
+    printf("%d\n",a);
+
+    char c = 255;
+    int i = 255;
+    printf("c=%d,i=%d\n",c, i);
+
+    unsigned int maxInt = 0;
+    maxInt = 0-1;
+    printf("最大的int:%ud", maxInt);
+    return 0;
+}
