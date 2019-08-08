02a08ff941a87db6a055d531cfb612aaf0489211
luhha <luhh18@outlook.com>
2019-08-08 17:08:36 +0800

    js事件基本

--- 03javascript/00test/testVar.js
+++ 03javascript/00test/testVar.js
@@ -3,4 +3,16 @@ console.log(`current ${__filename}, __dirname:${__dirname}`)
 console.log(`call other directory justModule.test:`)
 test.test();
 
-console.log(module.parent.filename);
+// console.log(module.parent.filename);
+
+class TestC {
+    test(){
+        console.log(this.constructor.name);
+    }
+}
+function test2(){
+    console.log(this.constructor);
+}
+let a = new TestC();
+a.test();
+test2();
